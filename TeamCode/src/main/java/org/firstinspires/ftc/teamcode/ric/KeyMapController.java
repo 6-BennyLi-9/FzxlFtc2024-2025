7202c6e299d29fb3fefcb781be7b08187307f63c
Benny0Li <17826002105@163.com>
2024-10-08 12:40:39 +0800

    copy ric

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ric/KeyMapController.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.ric;
+
+import androidx.annotation.NonNull;
+
+import org.firstinspires.ftc.teamcode.utils.annotations.UserRequirementFunctions;
+
+/**
+ * @see Robot
+ * @see KeyMap
+ * */
+public abstract class KeyMapController {
+	@UserRequirementFunctions
+	public abstract void KeyMapOverride(@NonNull KeyMap keyMap);
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ric/VoidKeyMapController.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.ric;
+
+import androidx.annotation.NonNull;
+
+public class VoidKeyMapController extends KeyMapController{
+	@Override
+	public void KeyMapOverride(@NonNull KeyMap keyMap) {}
+}
