2481a43d1b0fb8d4745da40a25ddc8f60a5ae556
BennyLi <bennyli2011@163.com>
2024-11-08 22:49:40 +0800

    原数据库删除

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actions/Action.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.actions;
-
-public interface Action {
-	/**
-	 * @return 该 {@code Action} 块是否结束
-	 */
-	boolean run();
-	default Action next(){return new Actions.FinalNodeAction();}
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actions/PriorityAction.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.actions;
-
-public interface PriorityAction extends Action{
-	/**
-	 * @return 优先级，数值越大优先级越高
-	 */
-	long getPriorityCode();
-}
