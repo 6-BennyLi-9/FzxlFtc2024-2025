1d253ca102ce21b3e70511d58f6ea256d89feea9
BennyLi <bennyli2011@163.com>
2024-11-10 15:02:00 +0800

    TelemetryClient 大回归

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/client/TelemetryClient.java
@@ -0,0 +1,157 @@
+package org.firstinspires.ftc.teamcode.client;
+
+import android.util.Pair;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Vector;
+
+public class TelemetryClient {
+	private final Telemetry telemetry;
+	protected final Map < String , Pair< String , Integer >> data;
+	protected int ID;
+	public static boolean showIndex;
+	private static TelemetryClient instanceTelemetryClient;
+
+	public TelemetryClient(final Telemetry telemetry){
+		this.telemetry=telemetry;
+		this.data = new HashMap<>();
+		instanceTelemetryClient =this;
+	}
+
+	public static TelemetryClient getInstance(){
+		return instanceTelemetryClient;
+	}
+
+	public static void registerInstance(final Telemetry telemetry){
+		instanceTelemetryClient=new TelemetryClient(telemetry);
+	}
+
+	public void clear(){
+		this.data.clear();
+		this.update();
+	}
+
+	/**
+	 * 注意：这是新的Data
+	 */
+	public TelemetryClient addData(final String key, final String val){
+		++ this.ID;
+		this.data.put(key,new Pair<>(val, this.ID));
+		this.update();
+
+		return this;
+	}
+	/**
+	 * 注意：这是新的Data
+	 */
+	public TelemetryClient addData(final String key, final Object val){
+		return addData(key,String.valueOf(val));
+	}
+	/**
+	 * @throws RuntimeException 如果未能找到key所指向的值，将会抛出异常
+	 */
+	public TelemetryClient deleteData(final String key){
+		this.data.remove(key);
+		this.update();
+
+		return this;
+	}
+
+	/**
+	 * 自动创建新的行如果key所指向的值不存在
+	 */
+	public TelemetryClient changeData(final String key, final String val){
+		if(this.data.containsKey(key)){
+			this.data.replace(key,new Pair<>(val, (Objects.requireNonNull(this.data.get(key))).second));
+		}else{
+			this.addData(key, val);
+		}
+		this.update();
+
+		return this;
+	}
+	/**
+	 * 自动创建新的行如果key所指向的值不存在
+	 */
+	public TelemetryClient changeData(final String key, final Object val){
+		return changeData(key,String.valueOf(val));
+	}
+
+	public TelemetryClient addLine(final String key){
+		++ this.ID;
+		this.data.put(key,new Pair<>("", this.ID));
+		this.update();
+
+		return this;
+	}
+	public TelemetryClient addLine(final Object key){
+		return addLine(String.valueOf(key));
+	}
+	/**
+	 * @throws RuntimeException 如果未能找到key所指向的值，将会抛出异常
+	 */
+	public TelemetryClient deleteLine(final String key){
+		this.data.remove(key);
+		this.update();
+
+		return this;
+	}
+
+	/**
+	 * 将key行替代为val，自动创建新的行如果key所指向的值不存在
+	 * @param oldData 目标行
+	 * @param newData 替换行
+	 */
+	public TelemetryClient changeLine(final String oldData, final String newData){
+		final int cache =Objects.requireNonNull(this.data.get(oldData)).second;
+		this.data.remove(oldData);
+		this.data.put(newData,new Pair<>("",cache));
+		this.update();
+
+		return this;
+	}
+
+	public static boolean sortDataInTelemetryClientUpdate=true;
+
+	public void update(){
+		if(sortDataInTelemetryClientUpdate) {
+			final Vector <Pair <Integer, String>> outputData = new Vector <>();
+			for (final Map.Entry <String, Pair <String, Integer>> i : this.data.entrySet()) {
+				final String key = i.getKey();
+				final String val = i.getValue().first;
+				final Integer IDCache = i.getValue().second;
+				if (! Objects.equals(i.getValue().first, "")) {//line
+					outputData.add(new Pair <>(IDCache, key + ":" + val));
+				} else {//line
+					outputData.add(new Pair <>(IDCache, key));
+				}
+			}
+			outputData.sort(Comparator.comparingInt(x -> x.first));
+			for (final Pair <Integer, String> outputLine : outputData) {
+				if (showIndex) {
+					this.telemetry.addLine("[" + outputLine.first + "]" + outputLine.second);
+				} else {
+					this.telemetry.addLine(outputLine.second);
+				}
+			}
+			this.telemetry.update();
+		}else{
+			String cache;
+			for (final Map.Entry<String, Pair<String, Integer>> entry : this.data.entrySet()) {
+				final String                key = entry.getKey();
+				final Pair<String, Integer> val = entry.getValue();
+				cache= Objects.equals(val.first, "") ? val.first : key + ":" + val.first;
+				if (showIndex) {
+					this.telemetry.addLine("[" + val.second + "]" + cache);
+				} else {
+					this.telemetry.addLine(key + ":" + cache);
+				}
+			}
+		}
+	}
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Main.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.client.TelemetryClient;
 
 @TeleOp(name = "22232",group = "Main")
 public class Main extends OpMode {
@@ -14,13 +17,39 @@ public class Main extends OpMode {
 		robot=new Robot(hardwareMap);
 		robot.registerGamepad(gamepad1,gamepad2);
 		robot.initActions();
+		TelemetryClient.registerInstance(new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry()));
+
+		TelemetryClient.getInstance()
+				.addData("TPS","wait for start")
+				.addData("time","wait for start")
+				.addLine("=================");
+	}
+
+	/**单位：秒*/
+	public double start,lst;
+
+	@Override
+	public void init_loop() {
+		start=System.nanoTime()/ 1.0e9;
+		lst=start;
 	}
 
 	@Override
 	public void loop() {
+		final double now=System.nanoTime()/ 1.0e9;
+		TelemetryClient.getInstance()
+				.changeData("time",now-start)
+				.changeData("TPS",1/(now-lst));
+		lst=now;
+
 		robot.runThread();
 
 		robot.operateThroughGamepad();
 		robot.driveThroughGamepad();
+	}
+
+	@Override
+	public void stop() {
+		TelemetryClient.getInstance().clear();
 	}
 }
