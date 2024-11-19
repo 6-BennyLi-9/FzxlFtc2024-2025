1019b920031150dea6b112f1f2f989dfce27cb15
BennyLi <bennyli2011@163.com>
2024-11-19 19:01:42 +0800

    Util 回归了

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Util.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import static org.firstinspires.ftc.teamcode.HardwareConstants.*;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.actions.packages.ActionPackage;
+import org.firstinspires.ftc.teamcode.actions.utils.StatementAction;
+import org.firstinspires.ftc.teamcode.actions.utils.ThreadedAction;
+
+/**
+ * 适配于自动程序的 {@code Robot}
+ * @see Robot
+ */
+public class Util{
+	private final ActionPackage thread;
+
+	public Util(){
+		thread=new ActionPackage();
+	}
+
+	//PlaceOption
+	public Util decant(){
+		thread.add(new StatementAction(()-> place.setPosition(1)));
+		return this;
+	}
+	public Util boxRst(){
+		thread.add(new StatementAction(()-> place.setPosition(0)));
+		return this;
+	}
+
+	//ClipOption
+	public Util openClip(){
+		thread.add(new StatementAction(()-> clip.setPosition(0)));
+		return this;
+	}
+	public Util closeClip(){
+		thread.add(new StatementAction(()-> clip.setPosition(0.5)));
+		return this;
+	}
+
+	//IOTakesOption
+	public Util intake(){
+		thread.add(new StatementAction(()-> intake.setPosition(1)));
+		return this;
+	}
+	public Util outtake(){
+		thread.add(new StatementAction(()-> intake.setPosition(0)));
+		return this;
+	}
+	public Util stopIO(){
+		thread.add(new StatementAction(()-> intake.setPosition(0.5)));
+		return this;
+	}
+
+	//ArmOption
+	public Util displayArms(){
+		thread.add(new ThreadedAction(
+				new StatementAction(()-> leftArm.setPosition(0.3)),
+				new StatementAction(()-> rightArm.setPosition(0.3))
+		));
+		return this;
+	}
+	public Util armsIDLE(){
+		thread.add(new ThreadedAction(
+				new StatementAction(()-> leftArm.setPosition(0.86)),
+				new StatementAction(()-> rightArm.setPosition(0.86))
+		));
+		return this;
+	}
+	public Util armsToSafePosition(){
+		thread.add(new ThreadedAction(
+				new StatementAction(()-> leftArm.setPosition(0.75)),
+				new StatementAction(()-> rightArm.setPosition(0.75))
+		));
+		return this;
+	}
+
+	//ScaleOption
+	public Util scalesProbe(){
+		leftScale.setPosition(0.5);
+		rightScale.setPosition(1);
+		return this;
+	}
+	public Util scalesBack(){
+		leftScale.setPosition(1);
+		rightScale.setPosition(0.5);
+		return this;
+	}
+
+	public void runCached(){
+		thread.runTillEnd();
+	}
+}
