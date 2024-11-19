b2bf0aaa5f027645433d9c8b82499f914d8643ac
BennyLi <bennyli2011@163.com>
2024-11-19 19:19:42 +0800

    加入 ClassicLiftController

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/structure/ClassicLiftController.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.autonomous.structure;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.util.HardwareConstants;
+
+@Config
+public class ClassicLiftController extends AutonomousLiftController {
+	public static long staticAllowError;
+	public static long lowerErrorRange;
+	public static double   zeroPoseCalibrationPow,lowerCalibrationPow,higherCalibrationPow;
+	private double calibrateVal;
+	private boolean calibrationDone;
+
+	static {
+		staticAllowError=20;
+		lowerErrorRange=50;
+
+		zeroPoseCalibrationPow=1;
+		lowerCalibrationPow=0.3;
+		higherCalibrationPow=0.7;
+	}
+
+	protected ClassicLiftController(@NonNull final DcMotorEx target, final long targetPosition) {
+		super(target, targetPosition);
+	}
+
+	@Override
+	public void modify() {
+		if(0 == getTargetPosition()){
+			calibrateVal= !HardwareConstants.liftTouch.isPressed() ? 0 : - zeroPoseCalibrationPow;
+			calibrationDone= 0 == calibrateVal;
+			return;
+		}
+
+		if(staticAllowError > Math.abs(getErrorPosition())){
+			calibrateVal= 0;
+			calibrationDone=true;
+		}else if(lowerErrorRange > Math.abs(getErrorPosition())){
+			calibrateVal= lowerCalibrationPow * Math.signum(getErrorPosition());
+			calibrationDone=false;
+		}else{
+			calibrateVal= higherCalibrationPow * Math.signum(getErrorPosition());
+			calibrationDone=false;
+		}
+	}
+
+	@Override
+	public double getCalibrateVal() {
+		return calibrateVal;
+	}
+
+	@Override
+	public boolean getCalibrationDone() {
+		return calibrationDone;
+	}
+}
