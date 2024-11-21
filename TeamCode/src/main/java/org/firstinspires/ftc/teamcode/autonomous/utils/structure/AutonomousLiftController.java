8ba302af6d54790b78aa8202a400834175fa6701
Benny0Li <17826002105@163.com>
2024-11-21 12:19:29 +0800

    setPower(0)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/utils/structure/AutonomousLiftController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/utils/structure/AutonomousLiftController.java
@@ -33,6 +33,9 @@ public abstract class AutonomousLiftController extends LiftController {
 			targetLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 		}
 
+		if(getCalibrationDone()){
+			targetLift.setPower(0);
+		}
 		return !getCalibrationDone();
 	}
 
