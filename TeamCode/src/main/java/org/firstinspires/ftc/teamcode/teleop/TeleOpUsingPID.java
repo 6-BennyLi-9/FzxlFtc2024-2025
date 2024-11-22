de802a76e2363b9580953a76ff83a0b385f04cd0
Benny0Li <17826002105@163.com>
2024-11-22 12:12:12 +0800

    使用 super 优化

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpUsingPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpUsingPID.java
@@ -1,30 +1,15 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.client.TelemetryClient;
 import org.firstinspires.ftc.teamcode.structure.DriveOption;
-import org.firstinspires.ftc.teamcode.util.HardwareConstants;
-import org.firstinspires.ftc.teamcode.util.Robot;
 
 @TeleOp(name = "19419(using PID)",group = "Main")
 public class TeleOpUsingPID extends TeleOpCore{
 	@Override
 	public void init() {
+		super.init();
 		DriveOption.setDriveUsingPID(true);
-
-		HardwareConstants.sync(hardwareMap, true);
-		HardwareConstants.chassisConfig();
-		robot=new Robot();
-		robot.registerGamepad(gamepad1,gamepad2);
-		robot.initActions();
-		TelemetryClient.registerInstance(new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry()));
-
-		TelemetryClient.getInstance()
-				.addData("TPS","wait for start")
-				.addData("time","wait for start");
 	}
 }
