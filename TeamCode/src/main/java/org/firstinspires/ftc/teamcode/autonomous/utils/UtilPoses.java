2554fbdb8d6fab5b92c7b150cd5840af49b71456
Benny0Li <17826002105@163.com>
2024-11-22 16:40:44 +0800

    加入全部点位

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/utils/UtilPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/utils/UtilPoses.java
@@ -5,22 +5,38 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 @SuppressWarnings("unused")
 public enum UtilPoses {
 	;
-	//起始点位
 	public static Pose2d
-			BlueLeftStart=new Pose2d(12,62,Math.toRadians(-90)),
-			BlueRightStart=new Pose2d(-12,62,Math.toRadians(-90)),
-			RedLeftStart=new Pose2d(-12,-62,Math.toRadians(90)),
-			RedRightStart=new Pose2d(12,-62,Math.toRadians(90)),
+	BlueLeftStart=new Pose2d(12,62,Math.toRadians(-90)),
+	BlueRightStart=new Pose2d(-12,62,Math.toRadians(-90)),
+	RedLeftStart=new Pose2d(-12,-62,Math.toRadians(90)),
+	RedRightStart=new Pose2d(12,-62,Math.toRadians(90)),
 
-			BlueDecant=new Pose2d(55,55,Math.toRadians(-135)),
-			RedDecant=new Pose2d(-55,-55,Math.toRadians(135)),
+	BlueDecant=new Pose2d(55,55,Math.toRadians(-135)),
+	RedDecant=new Pose2d(-55,-55,Math.toRadians(135)),
 
-			BlueLeftSuspend=new Pose2d(BlueLeftStart.getX(),32,Math.toRadians(90)),
-			BlueRightSuspend=new Pose2d(BlueRightStart.getX(),32,Math.toRadians(90)),
-			RedLeftSuspend=new Pose2d(RedLeftStart.getX(),-32,Math.toRadians(90)),
-			RedRightSuspend=new Pose2d(RedRightStart.getX(),-32,Math.toRadians(90)),
+	BlueLeftSuspend=new Pose2d(BlueLeftStart.getX(),32,Math.toRadians(90)),
+	BlueRightSuspend=new Pose2d(BlueRightStart.getX(),32,Math.toRadians(90)),
+	RedLeftSuspend=new Pose2d(RedLeftStart.getX(),-32,Math.toRadians(90)),
+	RedRightSuspend=new Pose2d(RedRightStart.getX(),-32,Math.toRadians(90)),
 
-			BlueLeftSample1=new Pose2d(48,24,Math.toRadians(0)),
-			BlueLeftSample2=new Pose2d(44,22.5,Math.toRadians(0)),
-			BlueLeftSample3=new Pose2d(55,22.5,Math.toRadians(0));
+	BlueLeftSample1=new Pose2d(48,23,Math.toRadians(0)),
+	BlueLeftSample2=new Pose2d(50,BlueLeftSample1.getY(),Math.toRadians(0)),
+	BlueLeftSample3=new Pose2d(52,BlueLeftSample1.getY(),Math.toRadians(0)),
+
+	RedLeftSample1=new Pose2d(48,-23,Math.toRadians(0)),
+	RedLeftSample2=new Pose2d(50,RedLeftSample1.getY(),Math.toRadians(0)),
+	RedLeftSample3=new Pose2d(52,RedLeftSample1.getY(),Math.toRadians(0)),
+
+	BlueRightSample1=new Pose2d(-48,23,Math.toRadians(180)),
+	BlueRightSample2=new Pose2d(-50,BlueRightSample1.getY(),Math.toRadians(180)),
+	BlueRightSample3=new Pose2d(-52,BlueRightSample1.getY(),Math.toRadians(180)),
+
+	RedRightSample1=new Pose2d(-48,-23,Math.toRadians(180)),
+	RedRightSample2=new Pose2d(-50,RedRightSample1.getY(),Math.toRadians(180)),
+	RedRightSample3=new Pose2d(-52,RedRightSample1.getY(),Math.toRadians(180)),
+
+	BlueLeftPark=new Pose2d(30,12,Math.toRadians(180)),
+	BlueRightPark=new Pose2d(-30,12,Math.toRadians(0)),
+	RedLeftPark=new Pose2d(-30,-12,Math.toRadians(0)),
+	RedRightPark=new Pose2d(30,-12,Math.toRadians(180));
 }
