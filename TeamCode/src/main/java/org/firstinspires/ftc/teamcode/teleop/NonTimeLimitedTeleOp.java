3864f7ac67b0b4a133da856eeb558d7a15add596
Benny0Li <17826002105@163.com>
2024-11-26 13:37:23 +0800

    加入 NonTimeLimitedTeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/NonTimeLimitedTeleOp.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "19419(non-TLE)",group = "1_Utils")
+public class NonTimeLimitedTeleOp extends TeleOpCore{
+	@Override
+	public void init() {
+		super.init();
+		auto_terminate_when_TLE(false);
+	}
+}
