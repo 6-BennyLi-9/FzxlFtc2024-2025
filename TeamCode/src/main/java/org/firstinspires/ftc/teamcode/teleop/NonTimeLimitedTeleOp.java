d5766b002702e4d8a026688cc4692d94f9ddbbca
Benny0Li <17826002105@163.com>
2024-11-27 12:54:30 +0800

    弃用

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/NonTimeLimitedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/NonTimeLimitedTeleOp.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @Disabled
+@Deprecated
 @TeleOp(name = "19419(non-TLE)", group = "1_Utils")
 public class NonTimeLimitedTeleOp extends TeleOpCore {
 	@Override
