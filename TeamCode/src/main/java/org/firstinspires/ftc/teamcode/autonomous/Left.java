b88955f60676c57f4f596f11c0ed3bde803c33f2
Benny0Li <17826002105@163.com>
2024-11-29 17:59:11 +0800

    下课了。。。

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Left.java
@@ -27,7 +27,13 @@ public class Left extends IntegralAutonomous {
 				.lineToLinearHeading(LeftSample.plus(new Pose2d(0,0,Math.toRadians(23.5))))
 				.build());
 
-		registerTrajectory("decant",generateBuilder(LeftSample)
+		registerTrajectory("decant 1",generateBuilder(LeftSample)
+				.lineToLinearHeading(Decant)
+				.build());
+		registerTrajectory("decant 2",generateBuilder(LeftSample.plus(new Pose2d(0,0,Math.toRadians(-23))))
+				.lineToLinearHeading(Decant)
+				.build());
+		registerTrajectory("decant 3",generateBuilder(LeftSample.plus(new Pose2d(0,0,Math.toRadians(23.5))))
 				.lineToLinearHeading(Decant)
 				.build());
 
@@ -42,23 +48,20 @@ public class Left extends IntegralAutonomous {
 		utils.integralLiftUpPrepare().liftSuspendHighPrepare().runAsThread();
 		runTrajectory("suspend preload");
 		utils.liftSuspendHigh().runCached();
-		sleep(400);
+		sleep(500);
 		utils.openClip().waitMs(100).liftDown()
 				.integralIntakes().scaleOperate(0.82).runAsThread();
 
 		runTrajectory("get sample");
-		utils.displayArms().waitMs(600).integralIntakesEnding().waitMs(1500)
-//				.openClaw().waitMs(100).closeClaw().waitMs(100)
-//				.openClaw().waitMs(100).closeClaw().waitMs(100)
-//				.waitMs(400)
+		utils.displayArms().waitMs(600).integralIntakesEnding().waitMs(1400)
 				.openClaw()
-				.waitMs(100)
+				.waitMs(200)
 				.integralLiftUpPrepare().liftDecantHigh()
 				.runAsThread();
 		sleep(1000);
-		runTrajectory("decant");
+		runTrajectory("decant 1");
 
-		sleep(1000);
+		sleep(500);
 		utils.decant().waitMs(1300).integralLiftDownPrepare().liftDown()
 				.integralIntakes().rotateRightTurn(0.135).scaleOperate(0.87)
 				.runAsThread();
@@ -67,36 +70,32 @@ public class Left extends IntegralAutonomous {
 
 		sleep(1000);
 
-		utils.displayArms().waitMs(600).integralIntakesEnding().waitMs(1500)
-//				.openClaw().waitMs(100).closeClaw().waitMs(100)
-//				.openClaw().waitMs(100).closeClaw().waitMs(100)
-//				.waitMs(400)
+		utils.displayArms().waitMs(600).integralIntakesEnding().waitMs(1400)
 				.openClaw()
-				.waitMs(100)
+				.waitMs(200)
 				.integralLiftUpPrepare().liftDecantHigh()
 				.runAsThread();
 		sleep(1000);
-		runTrajectory("decant");
-		sleep(1000);
+		runTrajectory("decant 2");
+
+		sleep(500);
 		utils.decant().waitMs(1300).integralLiftDownPrepare().liftDown()
-				.waitMs(500).integralIntakes().rotateRightTurn(-0.135).scaleOperate(0.86)
+				.waitMs(500).integralIntakes().rotateRightTurn(-0.14).scaleOperate(0.86)
 				.runAsThread();
 		sleep(1000);
 		runTrajectory("to sample 2");
 
 		sleep(1000);
 
-		utils.displayArms().waitMs(600).integralIntakesEnding().waitMs(1500)
-//				.openClaw().waitMs(100).closeClaw().waitMs(100)
-//				.openClaw().waitMs(100).closeClaw().waitMs(100)
-//				.waitMs(400)
+		utils.displayArms().waitMs(600).integralIntakesEnding().waitMs(1400)
 				.openClaw()
-				.waitMs(100)
+				.waitMs(200)
 				.integralLiftUpPrepare().liftDecantHigh()
 				.runAsThread();
 		sleep(1000);
-		runTrajectory("decant");
-		sleep(1000);
+		runTrajectory("decant 3");
+
+		sleep(500);
 		utils.decant().waitMs(1300).integralLiftDownPrepare().liftDown()
 				.liftSuspendLv1()
 				.runAsThread();
