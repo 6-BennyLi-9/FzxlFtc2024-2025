5ffd578c9b8e7ee522e0f04c39396f6b4e6b1f68
Benny0Li <17826002105@163.com>
2024-11-29 12:09:02 +0800

    完成对吸取和 waitMs 函数的测试

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAutonomous.java
@@ -3,21 +3,20 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.util.ops.IntegralAutonomous;
-import org.firstinspires.ftc.teamcode.util.ops.UtilMng;
 
 @Autonomous(group = "9_Tests")
 public final class TestAutonomous extends IntegralAutonomous {
 	@Override
 	public void initialize() {
-		utils = new UtilMng();
+//		utils = new UtilMng();
 		utils.armsToSafePosition().runCached();
 	}
 
 	@Override
 	public void linear() {
-		utils.displayArms().openClaw().runCached();
-		sleep(1000);
-		utils.armsIDLE().closeClaw().runCached();
+		utils.integralLiftUpPrepare().liftDecantLow().waitMs(1000).liftDecantHigh().waitMs(1000).liftDown().runCached();
+		utils.integralIntakes().scaleOperate(0.82).waitMs(1000).integralIntakesEnding().runCached();
+
 		flagging_op_complete();
 	}
 }
