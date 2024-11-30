3e21bdd0848c681e06f8b72b6d0f5f982472a1b8
Benny0Li <17826002105@163.com>
2024-11-30 16:06:06 +0800

    Right 较为稳定2传递

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Right.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Right.java
@@ -1,6 +1,13 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
-import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.*;
+import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.GetSample;
+import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.RightSample;
+import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.RightSample1;
+import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.RightSample2;
+import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.RightSample3;
+import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.RightStart;
+import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.RightSuspend;
+import static java.lang.Math.toRadians;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -29,13 +36,13 @@ public class Right extends IntegralAutonomous {
 				.build());
 
 		registerTrajectory("turn 1",generateSequenceBuilder(RightSample1)
-				.turn(90)
+				.turn(toRadians(-90))
 				.build());
 		registerTrajectory("turn 2",generateSequenceBuilder(RightSample2)
-				.turn(90)
+				.turn(toRadians(-90))
 				.build());
 		registerTrajectory("turn 3",generateSequenceBuilder(RightSample3)
-				.turn(90)
+				.turn(toRadians(-90))
 				.build());
 
 		registerTrajectory("get sample",generateBuilder(RightSample)
@@ -52,7 +59,6 @@ public class Right extends IntegralAutonomous {
 				.lineToLinearHeading(RightSuspend.plus(new Pose2d(15,0)))
 				.build());
 
-
 		registerTrajectory("park",generateBuilder(RightSuspend)
 				.lineToLinearHeading(GetSample)
 				.build());
@@ -62,11 +68,45 @@ public class Right extends IntegralAutonomous {
 	public void linear() {
 		utils.integralLiftUpPrepare().liftSuspendHighPrepare().runAsThread();
 		runTrajectory("suspend preload");
-		utils.liftSuspendHigh().runCached();
-		sleep(400);
-		utils.openClip().waitMs(100).liftDown()
-				.integralIntakes().scaleOperate(0.82).runAsThread();
+		utils.liftSuspendHigh().waitMs(300).openClip().waitMs(100).liftDown().runAsThread();
 
+		sleep(600);
+		utils.scaleOperate(0.76).rotateRightTurn(0.2).runCached();
+
+		runTrajectory("to sample 1");
+		sleep(500);
+		utils.displayArms().waitMs(600).closeClaw().waitMs(300)
+				.armsIDLE().scaleOperate(0.95).waitMs(500).runAsThread();
+		sleep(660);
+		runTrajectory("turn 1");
+		utils.displayArms().waitMs(500).openClaw().waitMs(200).scaleOperate(0.76).armsIDLE().runAsThread();
+
+		sleep(1000);
+		runTrajectory("to sample 2");
+		sleep(500);
+		utils.displayArms().waitMs(800).closeClaw().waitMs(300)
+				.armsIDLE().scaleOperate(0.6).waitMs(500).runAsThread();
+		sleep(880);
+		runTrajectory("turn 2");
+		utils.scaleOperate(0.9)
+				.displayArms().waitMs(600).openClaw().waitMs(500).scaleOperate(0.77).armsIDLE().runAsThread();
+
+
+		sleep(1200);
+		runTrajectory("to sample 3");
+		sleep(500);
+		utils.displayArms().waitMs(800).closeClaw().waitMs(300)
+				.armsIDLE().scaleOperate(0.6).waitMs(500).runAsThread();
+		sleep(880);
+		runTrajectory("turn 3");
+		utils.scaleOperate(0.9)
+				.displayArms().waitMs(600).openClaw().waitMs(200)
+				.armsIDLE().scalesBack().armsIDLE().rotateToMid().runCached();
+
+		runTrajectory("get sample");
+//		utils.closeClaw();
+//		runTrajectory("turn 1");
+//		utils.openClaw();
 
 		flagging_op_complete();
 	}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/UtilPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/UtilPoses.java
@@ -19,8 +19,8 @@ public enum UtilPoses {
 
 			RightSample = new Pose2d(- 60, 46, toRadians(-90)),
 			RightSample1 = new Pose2d(- 36, 36, toRadians(-135)),
-			RightSample2 = new Pose2d(- 44.5, RightSample1.getY(), RightSample1.getHeading()),
-			RightSample3 = new Pose2d(- 53, RightSample1.getY(), RightSample1.getHeading()),
+			RightSample2 = new Pose2d(- 47, RightSample1.getY(), RightSample1.getHeading()),
+			RightSample3 = new Pose2d(- 52, RightSample1.getY(), RightSample1.getHeading()),
 
 			GetSample = new Pose2d(- 48, 61, toRadians(-90)),
 
