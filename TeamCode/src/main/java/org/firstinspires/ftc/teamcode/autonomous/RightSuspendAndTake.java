8cb2ef083466128089c25f9590971d0cabbf54aa
Benny0Li <17826002105@163.com>
2024-11-30 18:46:16 +0800

    加入 RightSuspendAndTake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RightSuspendAndTake.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "*Right(3悬挂+1夹取+1预载)", preselectTeleOp = "19419", group = "2_Betas")
+public class RightSuspendAndTake extends RightSuspend{
+	Pose2d cache;
+	@Override
+	public void initialize() {
+		super.initialize();
+		//override
+		cache=registerTrajectory("park",generateBuilder(UtilPoses.RightSuspend)
+				.lineToLinearHeading(UtilPoses.GetSample.plus(new Pose2d(2,-15)))
+				.build());
+
+		registerTrajectory("lst park",generateSequenceBuilder(cache)
+				.back(5)
+				.build());
+	}
+
+	@Override
+	public void linear() {
+		super.linear();
+//		angleCalibration(180,cache);
+		utils.integralIntakes().scalesProbe().displayArms().waitMs(600).integralIntakesEnding()
+				.waitMs(1200)
+				.openClaw().waitMs(100).closeClaw().waitMs(100)
+				.openClaw()
+				.armsToSafePosition().decant()
+				.runAsThread();
+		sleep(800);
+		runTrajectory("lst park");
+
+		flagging_op_complete();
+	}
+}
