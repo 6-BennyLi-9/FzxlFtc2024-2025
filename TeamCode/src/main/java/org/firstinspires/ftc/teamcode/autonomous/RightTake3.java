e375f08bb9fcde18086534eaa95593bef3992df1
Benny0Li <17826002105@163.com>
2024-12-07 18:46:32 +0800

    Disable for COMPETITION RightTake3

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RightTake3.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import static org.firstinspires.ftc.teamcode.autonomous.UtilPoses.GetSample;
+import static java.lang.Math.toRadians;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Right(1悬挂+2夹取)", group = "3_Specials")
+public class RightTake3 extends RightTake2 {
+	@Override
+	public void initialize() {
+		super.initialize();
+
+		//override
+		registerTrajectory("park",generateBuilder(UtilPoses.RightGetSecondSample)
+				.lineToLinearHeading(GetSample.plus(new Pose2d(0, 0, toRadians(- 90))))
+				.build());
+	}
+
+	@Override
+	public void linear() {
+		utils.integralLiftUpPrepare().liftSuspendHighPrepare().runAsThread();
+		runTrajectory("suspend preload");
+		utils.liftSuspendHigh().waitMs(300).openClip().waitMs(100).liftDown().integralIntakes().scaleOperate(scaleGetPosition).runAsThread();
+		sleep(600);
+
+		runTrajectory("get sample 1");
+		utils.displayArms().waitMs(600).closeClaw().waitMs(300).integralIntakesEnding().runCached();
+
+		sleep(900);
+		utils.openClaw().waitMs(100).closeClaw().waitMs(100).openClaw().waitMs(200).scaleOperate(scaleGetPosition).armsToSafePosition().waitMs(200).decant().runAsThread();
+		runTrajectory("get sample 2");
+		sleep(200);
+		utils.displayArms().waitMs(600).closeClaw().waitMs(300).integralIntakesEnding().runCached();
+
+		sleep(900);
+		utils.openClaw().waitMs(100).closeClaw().waitMs(100).openClaw().waitMs(200).armsToSafePosition().decant().runAsThread();
+		sleep(900);
+
+		runTrajectory("park");
+
+		flagging_op_complete();
+	}
+}
