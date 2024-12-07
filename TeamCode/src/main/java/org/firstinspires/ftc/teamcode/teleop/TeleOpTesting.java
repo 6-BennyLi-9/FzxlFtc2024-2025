859a64a072f84572119438e1ed57318225fa563f
Benny0Li <17826002105@163.com>
2024-12-07 18:46:23 +0800

    Disable for COMPETITION

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpTesting.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.structure.DriveOp;
 
+@Disabled
 @TeleOp(name = "19419(Beta)", group = "9_Beta")
 public class TeleOpTesting extends TeleOpCore {
 	@Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpUsingPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpUsingPID.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.structure.DriveOp;
 
+@Disabled
 @TeleOp(name = "19419(using PID)", group = "9_Beta")
 public class TeleOpUsingPID extends TeleOpCore {
 	@Override
