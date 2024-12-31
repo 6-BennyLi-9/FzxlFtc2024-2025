3e7d0b54d1fc9993ebb92c0ed95dab3a3cecffde
Benny0Li <17826002105@163.com>
2024-12-31 13:18:43 +0800

    加入 Tagger

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Tagger.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import androidx.annotation.NonNull;
+
+public class Tagger {
+	private long ID;
+
+	public long summonID(){
+		return ++ID;
+	}
+	public String summonID(@NonNull Object object){
+		return object.getClass().getSimpleName()+summonID();
+	}
+}
