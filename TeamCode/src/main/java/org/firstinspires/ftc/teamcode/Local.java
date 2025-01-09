309cc287199a455681cdac1b8efb435d596a2555
Benny0Li <17826002105@163.com>
2025-01-09 15:04:20 +0800

    加入更多方法

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Local.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Local.java
@@ -1,9 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
+import java.util.concurrent.Callable;
+
 public class Local {
 	public static void sleep(long millis){
 		try {
 			Thread.sleep(millis);
 		} catch (InterruptedException ignored) {}
+	}
+
+	public static <K> void waitForVal(Callable<K> function, K expect){
+		waitForVal (function,expect,60);
+	}
+	public static <K> void waitForVal(Callable<K> function, K expect, long flashMillis){
+		try {
+			while (function.call()!=expect){
+				sleep(flashMillis);
+			}
+		}catch (Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void runMultiRunnable(@NonNull Runnable... runnable){
+		for(Runnable current : runnable){
+			Global.coreThreads.add(new Thread(current));
+		}
+	}
+	public static void runMultiThreads(@NonNull Thread... threads){
+		for (Thread current : threads){
+			Global.coreThreads.add(current);
+		}
 	}
 }
