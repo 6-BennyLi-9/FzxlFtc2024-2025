3caadcf36dba55dceb55a3ad18f79189f52139bf
Benny0Li <17826002105@163.com>
2025-01-09 16:41:53 +0800

    测试异常集中营，正常

--- TeamCode/src/main/java/org/firstinspires/ftc/cores/teleop/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/cores/teleop/TestTeleOp.java
@@ -1,14 +1,14 @@
 package org.firstinspires.ftc.cores.teleop;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.cores.structure.DriveMode;
 import org.firstinspires.ftc.cores.structure.DriveOp;
+import org.firstinspires.ftc.teamcode.Global;
+import org.firstinspires.ftc.teamcode.Local;
 
-@Disabled
-@TeleOp(name = "19419(Beta)", group = "9_Beta")
-public class TeleOpTesting extends TeleOpCore {
+@TeleOp(name = "19419(Test)", group = "9_Beta")
+public class TestTeleOp extends TeleOpCore {
 	@Override
 	public void op_init() {
 		super.op_init();
@@ -18,5 +18,9 @@ public class TeleOpTesting extends TeleOpCore {
 	@Override
 	public void op_start() {
 		super.op_start();
+		Global.threadManager.add("exception-thrower",new Thread(()->{
+			Local.sleep(5000);
+			throw new RuntimeException("test");
+		}));
 	}
 }
