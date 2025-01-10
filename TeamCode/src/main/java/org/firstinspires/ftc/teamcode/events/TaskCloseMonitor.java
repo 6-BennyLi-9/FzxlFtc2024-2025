146ef153a35411daa898ee81b5a81faeae716f56
Benny0Li <17826002105@163.com>
2025-01-10 16:13:33 +0800

    修改 TaskCloseMonitor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/events/TaskCloseMonitor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/events/TaskCloseMonitor.java
@@ -3,14 +3,20 @@ package org.firstinspires.ftc.teamcode.events;
 import org.firstinspires.ftc.teamcode.Local;
 
 public final class TaskCloseMonitor extends Thread{
-	private final Thread argument;
+	public final Thread argument;
+	public final long timeLimit;
+
 	public TaskCloseMonitor(Thread argument){
+		this(argument,5000);
+	}
+	public TaskCloseMonitor(Thread argument,long timeLimit){
 		this.argument=argument;
+		this.timeLimit=timeLimit;
 	}
 
 	@Override
 	public void run() {
-		Local.sleep(5000);
+		Local.sleep(timeLimit);
 		if(argument.getState()!=State.TERMINATED){
 			argument.interrupt();
 		}
