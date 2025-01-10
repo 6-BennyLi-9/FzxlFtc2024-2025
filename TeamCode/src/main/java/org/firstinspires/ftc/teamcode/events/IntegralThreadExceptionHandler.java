af8508fd8744b9dc670065edfad8d9f2ee0355d4
Benny0Li <17826002105@163.com>
2025-01-10 16:11:11 +0800

    加入 ForceStopException 的处理方法

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/events/IntegralThreadExceptionHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/events/IntegralThreadExceptionHandler.java
@@ -4,14 +4,24 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerImpl;
+
 import org.firstinspires.ftc.cores.eventloop.IntegralAutonomous;
 import org.firstinspires.ftc.cores.eventloop.IntegralOpMode;
 import org.firstinspires.ftc.cores.eventloop.TerminateReason;
 import org.firstinspires.ftc.teamcode.Global;
 
-public class ThreadExceptionHandler implements Thread.UncaughtExceptionHandler {
+public class IntegralThreadExceptionHandler implements Thread.UncaughtExceptionHandler {
 	@Override
 	public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
+		if (e instanceof OpModeManagerImpl.ForceStopException){
+			if (Global.currentOpmode instanceof IntegralAutonomous) {
+				((IntegralOpMode) Global.currentOpmode).sendTerminateSignal(TerminateReason.UserActions);
+			}else{
+				Global.currentOpmode.terminateOpModeNow();
+			}
+		}
+
 		Log.e("Error","OpMode Terminated By Exception",e);
 		if (Global.currentOpmode instanceof IntegralAutonomous) {
 			((IntegralOpMode) Global.currentOpmode).sendTerminateSignal(TerminateReason.UncaughtException, (Exception) e);
