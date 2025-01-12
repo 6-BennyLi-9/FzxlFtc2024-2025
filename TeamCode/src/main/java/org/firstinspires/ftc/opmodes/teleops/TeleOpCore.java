c7cd397d5b62d7652ed35648c1ec49646ddca8ed
Benny0Li <17826002105@163.com>
2025-01-12 14:25:23 +0800

    注释打印 IMU 数据的方法

--- TeamCode/src/main/java/org/firstinspires/ftc/opmodes/teleops/TeleOpCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/opmodes/teleops/TeleOpCore.java
@@ -25,7 +25,7 @@ public class TeleOpCore extends IntegralTeleOp {
 		//主程序结束
 
 		robot.printActions();
-		robot.printIMUVariables();
+		//		robot.printIMUVariables();
 		//		HardwareDatabase.printVoltages();
 		//		GamepadRequestMemories.printValues();
 
