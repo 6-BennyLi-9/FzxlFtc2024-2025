a5edb66fdb33204da5c6255a5a6890f5ae35ab09
Benny0Li <17826002105@163.com>
2025-01-13 13:06:02 +0800

    放弃

--- TeamCode/src/main/java/opensource.sh
+++ TeamCode/src/main/java/opensource.sh
@@ -5,7 +5,9 @@ git config --global user.name "Github Actions"
 git config --global user.email "actions@github.com"
 cat ~/.git/config
 
-git clone https://github.com/6-BennyLi-9/FtcStudio.git
+git init
+git remote add origin https://github.com/6-BennyLi-9/F
+git branch -m opensource
 git add org/betastudio/ftc/.
 git add org/firstinspires/ftc/teamcode/.
 git commit -m "auto update"
