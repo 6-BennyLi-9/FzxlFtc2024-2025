a98ef79b08f56f5741bbc984c309d23b6f0d2990
Benny0Li <17826002105@163.com>
2025-01-13 12:52:07 +0800

    更新 opensource.sh

--- TeamCode/src/main/java/opensource.sh
+++ TeamCode/src/main/java/opensource.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-git config --global user.email  "17826002105@163.com"
-git config --global user.name   "MingYuan Li"
+git config --global user.name "Github Actions"
+git config --global user.email "actions@github.com"
 
 git init
 git branch -m main
-git remote add origin https://github.com/6-BennyLi-9/FtcStudio.git
+git remote add origin git@github.com:6-BennyLi-9/FtcStudio.git
 git pull origin
 git add org/betastudio/ftc/.
 git add org/firstinspires/ftc/teamcode/.
