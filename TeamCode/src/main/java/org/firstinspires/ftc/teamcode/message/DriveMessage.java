3ea29f25f02ff5a08dbe3a38b025188ed9f7f062
Benny0Li <17826002105@163.com>
2025-01-15 14:43:05 +0800

    feat: 新增DriveMessage类继承DriveBufMessage

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/message/DriveMessage.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.message;
+
+public class DriveMessage extends DriveBufMessage{
+	public DriveMessage(double bufX, double bufY, double bufTurn) {
+		super(bufX, bufY, bufTurn);
+	}
+}
