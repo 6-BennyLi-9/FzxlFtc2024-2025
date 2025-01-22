20e6319792991cf56b210ef90919205fc9bf443f
BennyLi <bennyli2011@163.com>
2025-01-22 16:38:44 +0800

    refactor: 更新接口泛型约束以继承Message类

--- TeamCode/src/main/java/org/betastudio/ftc/interfaces/MessagesProcessRequired.java
+++ TeamCode/src/main/java/org/betastudio/ftc/interfaces/MessagesProcessRequired.java
@@ -2,9 +2,11 @@ package org.betastudio.ftc.interfaces;
 
 import androidx.annotation.NonNull;
 
+import org.firstinspires.ftc.teamcode.message.Message;
+
 import java.util.concurrent.Callable;
 
-public interface MessagesProcessRequired <K> extends Callable<K> {
+public interface MessagesProcessRequired <K extends Message> extends Callable<K> {
 	void sendRequest(@NonNull K message);
 	default K call() {return null;}
 }
