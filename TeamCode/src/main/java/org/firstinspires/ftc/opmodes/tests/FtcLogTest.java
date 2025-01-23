98118dc4d80e75a549c0d0337f6574f032be8bc9
BennyLi <bennyli2011@163.com>
2025-01-23 14:40:13 +0800

    fix: 修改update选项调用方法

--- TeamCode/src/main/java/org/firstinspires/ftc/opmodes/tests/FtcLogTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/opmodes/tests/FtcLogTest.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.opmodes.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.betastudio.ftc.client.BaseMapClient;
+import org.betastudio.ftc.client.BranchThreadClient;
 import org.betastudio.ftc.client.Client;
 import org.betastudio.ftc.client.ViewMode;
 import org.betastudio.ftc.log.FtcLogTunnel;
@@ -10,9 +10,8 @@ import org.betastudio.ftc.log.FtcLogTunnel;
 public class FtcLogTest extends LinearOpMode {
 	@Override
 	public void runOpMode() throws InterruptedException {
-		final Client client =new BaseMapClient(telemetry);
+		final Client client =new BranchThreadClient(telemetry);
 		client.configViewMode(ViewMode.LOG);
-		client.setAutoUpdate(true);
 		FtcLogTunnel.clear();
 		FtcLogTunnel.MAIN.report("Op In INIT");
 
