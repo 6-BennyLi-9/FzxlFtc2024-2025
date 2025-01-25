7413c4e3f85a0efbbcf7eb244a4f3cbb1b6672bd
BennyLi <bennyli2011@163.com>
2025-01-25 09:12:20 +0800

    style(core): 增加枚举项的注释

--- TeamCode/src/main/java/org/betastudio/ftc/client/UpdateConfig.java
+++ TeamCode/src/main/java/org/betastudio/ftc/client/UpdateConfig.java
@@ -1,5 +1,16 @@
 package org.betastudio.ftc.client;
 
 public enum UpdateConfig {
-	AUTO_UPDATE_WHEN_OPTION_PUSHED, MANUAL_UPDATE_REQUESTED, THREAD_REQUIRED,
+	/**
+	 * 在对 Client 执行操作时自动更新，注意显示日志的时候是不会自动更新的
+	 */
+	AUTO_UPDATE_WHEN_OPTION_PUSHED,
+	/**
+	 * 需要手动更新
+	 */
+	MANUAL_UPDATE_REQUESTED,
+	/**
+	 * 在后台执行
+	 */
+	THREAD_REQUIRED,
 }
