2a9c89304160bf59462e64003d52aea86ca112fd
BennyLi <bennyli2011@163.com>
2025-02-05 11:18:19 +0800

    refactor: 使用FtcExecutorService替代ExecutorService

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Global.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Global.java
@@ -3,26 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.betastudio.ftc.thread.FtcExecutorService;
+import org.betastudio.ftc.thread.FtcThreadPool;
 import org.betastudio.ftc.ui.client.Client;
 import org.betastudio.ftc.ui.log.FtcLogTunnel;
 import org.firstinspires.ftc.teamcode.events.SystemMonitor;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
 public final class Global {
-	public static ExecutorService threadService = new ThreadPoolExecutor(16, 32, 5000L, TimeUnit.MILLISECONDS, new ArrayBlockingQueue <>(1024), Executors.defaultThreadFactory(), (r, executor) -> {
-		r.run();
-		FtcLogTunnel.MAIN.report("Release rejected runnable: "+r);
-	});
-	public static Gamepad       gamepad1, gamepad2;
-	public static  RunMode runMode;
-	public static  OpMode  currentOpmode;
-	public static  Client  client;
-	private static boolean auto_create_monitor = true;
+	public static  Gamepad            gamepad1, gamepad2;
+	public static  FtcExecutorService threadService       = new FtcThreadPool();
+	public static  RunMode            runMode;
+	public static  OpMode             currentOpmode;
+	public static  Client             client;
+	private static boolean            auto_create_monitor = true;
 
 	public static void registerGamepad(final Gamepad gamepad1, final Gamepad gamepad2) {
 		Global.gamepad1 = gamepad1;
@@ -30,7 +23,7 @@ public final class Global {
 	}
 
 	public static void prepareCoreThreadPool() {
-		FtcLogTunnel.MAIN.report("Shutdown tasks of "+threadService.shutdownNow().size());
+		FtcLogTunnel.MAIN.report("Shutdown tasks of " + threadService.shutdownNow().size());
 
 		if (auto_create_monitor) {
 			createMonitor();
