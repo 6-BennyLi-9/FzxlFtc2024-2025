7f56f4977fdc1325d62648c5b70b12ae1673f821
Benny_Li <lmy20170914@163.com>
2025-02-19 14:05:17 +0800

    save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearEventMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearEventMode.java
@@ -4,6 +4,7 @@ import static org.firstinspires.ftc.teamcode.Utils.pushIn;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.MarkerCallback;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Const;
@@ -71,4 +72,19 @@ public abstract class LinearEventMode extends LinearOpMode {
 	public abstract void initialize();
 	@Const
 	public abstract Pose2d getInitialPoseEstimate();
+
+	/**
+	 * @return 按下 gamepad1.a 来激活断点
+	 */
+	public MarkerCallback newBreakpoint(){
+		return () -> {
+			try {
+				while (! gamepad1.a){
+					sleep(50);
+				}
+			} catch (Exception e) {
+				Thread.currentThread().interrupt();
+			}
+		};
+	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eventloop/RightNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eventloop/RightNew.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.eventloop;
 
+import static org.firstinspires.ftc.teamcode.RearLiftLocation.down;
+import static org.firstinspires.ftc.teamcode.RearLiftLocation.middle;
 import static org.firstinspires.ftc.teamcode.Utils.pushIn;
 import static org.firstinspires.ftc.teamcode.Utils.pushOut;
 import static java.lang.Math.toRadians;
@@ -8,11 +10,10 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.LinearEventMode;
-import org.firstinspires.ftc.teamcode.RearLiftLocation;
 
 @Autonomous(group = Character.MAX_VALUE + "beta", preselectTeleOp = "挂样本")
 public class RightNew extends LinearEventMode {
-	public static final int GET_SAMPLES_TIMES = 3;
+	public static final int GET_SAMPLES_TIMES = 2;
 	public static       int DELTA_GET_INCH    = -11;
 	public static       int SUSPEND_TIMES     = 4;
 	public static       int DELTA_PLACE_INCH  = -3;
@@ -20,7 +21,8 @@ public class RightNew extends LinearEventMode {
 
 	@Override
 	public void initialize() {
-		Pose2d originGetBlueSample   = new Pose2d(-36, 60, toRadians(- 135));
+		Pose2d originGetBlueSample   = new Pose2d(- 30, 43, toRadians(- 135));
+		Pose2d originPutBlueSample   = new Pose2d(- 30, 45, toRadians(100));
 		Pose2d toGetSuspendSample    = new Pose2d(- 43, 55, toRadians(- 90));
 		Pose2d toSuspendGottenSample = new Pose2d(- 4, 21 + SUSPEND_DIFF, toRadians(- 80));
 
@@ -30,27 +32,40 @@ public class RightNew extends LinearEventMode {
 					.addDisplacementMarker(()->{
 						utils.setPushPose(pushOut);
 						utils.claw_rotate_rst(false);    //翻转下去，打开
+						utils.rotate.setPosition(0.8);
 						sleep(500);
 						utils.clawOperation(false);      //夹住
 						sleep(200);
-
-						service.execute(()->{
-							Thread.yield();
-							utils.setPushPose(pushIn);
-						});
 					})
-					.turn(Math.toRadians(- 90))
-					.addDisplacementMarker(()-> utils.clawOperation(true));
+					.lineToSplineHeading(originPutBlueSample.plus(new Pose2d(DELTA_GET_INCH * i)))
+					.addDisplacementMarker(()-> {
+						sleep(500);
+						utils.clawOperation(true);
+					});
 		}
 
-		MAIN_BUILDER.addDisplacementMarker(()-> utils.claw_rotate(true));
+		MAIN_BUILDER
+				.lineToLinearHeading(toGetSuspendSample)
+				.addDisplacementMarker(()-> {
+					utils.claw_rotate(true);
+					utils.setPushPose(pushIn);
+
+					sleep(150);
+
+					service.execute(()->{
+						utils.clipOperation(false);    //夹住第一个
+						sleep(300);
+						utils.setRearLiftPosition(middle);
+						utils.armOperationR(false);
+					});
+				});
 
 		for (int i = 0 ; i < SUSPEND_TIMES ; i++) {
 			MAIN_BUILDER
 					.lineTo(toSuspendGottenSample.plus(new Pose2d(DELTA_PLACE_INCH * i)).vec())
 					.forward(SUSPEND_DIFF)
 					.addDisplacementMarker(()-> service.execute(()->{
-						utils.setRearLiftPosition(RearLiftLocation.down); //回电梯
+						utils.setRearLiftPosition(down); //回电梯
 						sleep(150);
 						utils.clipOperation(true);
 						utils.armOperationR(true);       //翻转手臂
@@ -62,7 +77,7 @@ public class RightNew extends LinearEventMode {
 						service.execute(()->{
 							utils.clipOperation(false);    //夹住
 							sleep(300);
-							utils.setRearLiftPosition(RearLiftLocation.middle);
+							utils.setRearLiftPosition(middle);
 							utils.armOperationR(false);
 						});
 					});
@@ -71,6 +86,6 @@ public class RightNew extends LinearEventMode {
 
 	@Override
 	public Pose2d getInitialPoseEstimate() {
-		return new Pose2d(- 35, 58, toRadians(- 90));
+		return new Pose2d(- 35, 58, toRadians(- 135));
 	}
 }
