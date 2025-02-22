9da6751463c3f243a96d325403a98484f8e8369d
Benny_Li <lmy20170914@163.com>
2025-02-22 08:26:53 +0800

    inline

--- TeamCode/src/main/java/org/betastudio/ftc/ui/client/InfinityLoopThread.java
+++ TeamCode/src/main/java/org/betastudio/ftc/ui/client/InfinityLoopThread.java
@@ -1,5 +1,6 @@
 package org.betastudio.ftc.ui.client;
 
+import org.betastudio.ftc.action.Action;
 import org.betastudio.ftc.util.entry.ThreadEx;
 
 public class InfinityLoopThread extends Thread implements ThreadEx {
@@ -17,5 +18,25 @@ public class InfinityLoopThread extends Thread implements ThreadEx {
 	@Override
 	public void closeTask() {
 		action.closeTask();
+	}
+
+	public static final class InfinityLoopAction implements Action, ThreadEx {
+		private final Runnable runnable;
+		private       boolean  interrupted;
+
+		public InfinityLoopAction(final Runnable runnable) {
+			this.runnable = runnable;
+		}
+
+		@Override
+		public boolean activate() {
+			runnable.run();
+			return ! interrupted;
+		}
+
+		@Override
+		public void closeTask() {
+			interrupted = true;
+		}
 	}
 }
