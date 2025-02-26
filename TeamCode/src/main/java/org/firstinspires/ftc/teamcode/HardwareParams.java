b467fc37cecd4c680d385d030d3e878d3c926546
Benny_Li <lmy20170914@163.com>
2025-02-26 14:18:56 +0800

    参数调整

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareParams.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareParams.java
@@ -9,13 +9,13 @@ public final class HardwareParams {
 	public static final double clawOpen   = 0.32;
 	public static final double clipOn     = 0.79;  //0.79
 	public static final double clipOpen   = 0.52;
-	public static final double armDown    = 0.92;  //翻转去夹0.16 0.89
-	public static final double armMiddle  = 0.83;  //翻转去挂
-	public static final double armUp      = 0.41;  //翻转去挂0.43
+	public static final double armDown    = 0.94;  //翻转去夹0.16 0.89
+	public static final double armMiddle  = 0.87;  //翻转去挂
+	public static final double armUp      = 0.45;  //翻转去挂0.43
 	public static final double upTurnUp   = 0.41;  //翻转去放0.25
 	public static final double upTurnDown = 0.79;  //翻转去夹0.77
-	public static final double armPut     = 0.90;  //翻转去夹上提0.56,0.84，0.93
-	public static final double armGet     = 0.20;  //0.16,挂边框0.14翻转去挂0.20
+	public static final double armPut     = 0.92;  //翻转去夹上提0.56,0.84，0.93
+	public static final double armGet     = 0.22;  //0.16,挂边框0.14翻转去挂0.20
 	public static final double upTurnGet  = 0.8;  //翻转去夹0.78,挂边框0.58
 	public static final double upTurnPut  = 0.16;  //翻转去挂0.05,上提0.67，0.16
 
@@ -23,11 +23,11 @@ public final class HardwareParams {
 	//            自动程序
 	//==============================
 
-	public static final double armUpL        = 0.43;
-	public static final double armDownMiddle = 0.87;
-	public static final double armDownL      = 0.92;
-	public static final double armUpR        = 0.90;
-	public static final double armDownR      = 0.20;
+	public static final double armUpL        = 0.45;
+	public static final double armDownMiddle = 0.89;
+	public static final double armDownL      = 0.94;
+	public static final double armUpR        = 0.92;
+	public static final double armDownR      = 0.22;
 	public static final double upTurnUpL     = 0.34;  //翻转去夹0.23
 	public static final double upTurnDownL   = 0.80;  //翻转去挂
 	public static final double upTurnUpR     = 0.16;  //翻转去夹0.16
