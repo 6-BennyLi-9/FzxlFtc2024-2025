3ce5b8f5f0111351aa0b3e5f6ce04ffe54902a9e
Benny0Li <17826002105@163.com>
2025-02-28 15:26:51 +0800

    移除多余的操作

--- TeamCode/src/main/java/org/betastudio/ftc/action/ActionImpl.java
+++ TeamCode/src/main/java/org/betastudio/ftc/action/ActionImpl.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Callable;
 /**
  * 子类只需调用 {@link #setAction(Callable)}并重写 {@link #paramsString()}即可
  */
-public class ActionImpl extends Thread implements Action, ThreadEx {
+public class ActionImpl implements Action, ThreadEx {
 	private Callable <Boolean> action;
 	private boolean isStopRequested;
 
