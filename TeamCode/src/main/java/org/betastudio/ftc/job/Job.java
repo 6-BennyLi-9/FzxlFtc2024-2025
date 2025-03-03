c81688f0389c9491f3b58daf038350b25e4c2524
Benny0Li <17826002105@163.com>
2025-03-03 11:26:45 +0800

    删除多余的依赖，准备重构 Job

--- TeamCode/src/main/java/org/betastudio/ftc/job/Job.java
+++ TeamCode/src/main/java/org/betastudio/ftc/job/Job.java
@@ -1,12 +1,16 @@
 package org.betastudio.ftc.job;
 
+import org.betastudio.ftc.action.Action;
+
 import java.util.Collection;
 
-public interface Job extends Runnable{
-	Collection<Job> getDependencies();
+public interface Job extends Action {
+	Collection <Job> getDependencies();
 	void addDependency(Job job);
 	void removeDependency(Job job);
 
 	String getName();
 	void setName(String name);
+
+	boolean isParallel();
 }
