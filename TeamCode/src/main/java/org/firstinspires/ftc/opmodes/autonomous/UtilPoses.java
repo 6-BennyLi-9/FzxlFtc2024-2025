ca81dd0be4fa892b4f02c3d0d2882e52a96be580
Benny0Li <17826002105@163.com>
2025-03-07 11:39:58 +0800

    修复错误优化

--- TeamCode/src/main/java/org/firstinspires/ftc/opmodes/autonomous/UtilPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/opmodes/autonomous/UtilPoses.java
@@ -12,23 +12,9 @@ public final class UtilPoses {
 
 	public static final Pose2d LeftSuspend  = new Pose2d(LeftStart.getX(), 32, toRadians(90));
 	public static final Pose2d RightSuspend = new Pose2d(RightStart.getX(), 32, toRadians(90));
-
 	public static final Pose2d LeftSample = new Pose2d(59, 50, toRadians(- 90));
-
-	public static Pose2d//right suspend
-	RightSample = new Pose2d(- 60, 46, toRadians(- 90));
-
-	public static final Pose2d//right take
-						 RightFirstSample  = new Pose2d(- 36, 36, toRadians(- 135));
-	public static       Pose2d RightSecondSample = new Pose2d(- 47, RightFirstSample.getY(), RightFirstSample.getHeading());
-	public static Pose2d RightThirdSample = new Pose2d(- 55.5, RightFirstSample.getY(), RightFirstSample.getHeading());
-
-	public static final Pose2d//right take 2
-							   RightGetFirstSample  = new Pose2d(- 49.5, 47.5, toRadians(- 90));
+	public static final Pose2d RightGetFirstSample  = new Pose2d(- 49.5, 47.5, toRadians(- 90));
 	public static final Pose2d RightGetSecondSample = new Pose2d(- 60, RightGetFirstSample.getY(), RightGetFirstSample.getHeading());
-
 	public static final Pose2d GetSample = new Pose2d(- 50, 61, toRadians(- 90));
-
 	public static final Pose2d LeftParkPrepare  = new Pose2d(36, 10, toRadians(0));
-	public static       Pose2d RightParkPrepare = new Pose2d(- 40, 12, toRadians(180));
 }
