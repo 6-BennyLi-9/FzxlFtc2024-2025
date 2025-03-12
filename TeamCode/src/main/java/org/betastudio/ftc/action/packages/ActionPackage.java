49f204e51945d2bcb15fa027e0786890e6fa12b2
Benny0Li <17826002105@163.com>
2025-03-12 14:31:32 +0800

    加入 LinkedActionPackage

--- TeamCode/src/main/java/org/betastudio/ftc/action/packages/ActionPackage.java
+++ TeamCode/src/main/java/org/betastudio/ftc/action/packages/ActionPackage.java
@@ -3,12 +3,10 @@ package org.betastudio.ftc.action.packages;
 import org.betastudio.ftc.action.Action;
 import org.betastudio.ftc.action.PriorityAction;
 
-public interface ActionPackage {
+public interface ActionPackage extends Action {
 	void add(PriorityAction action);
 
 	void add(Action action);
-
-	boolean activate();
 
 	void activateTillEnd();
 }
