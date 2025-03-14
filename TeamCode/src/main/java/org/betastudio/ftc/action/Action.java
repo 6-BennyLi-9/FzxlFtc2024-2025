d2835146680cb7311a1583b67974ea9600f0ad4f
Benny0Li <17826002105@163.com>
2025-03-14 15:41:26 +0800

    接入 Interfaces.Countable

--- TeamCode/src/main/java/org/betastudio/ftc/action/Action.java
+++ TeamCode/src/main/java/org/betastudio/ftc/action/Action.java
@@ -1,10 +1,12 @@
 package org.betastudio.ftc.action;
 
+import org.betastudio.ftc.Interfaces;
+
 /**
  * 基本的模块化程序块，可以应用于多方面的维护需求
  */
 @FunctionalInterface
-public interface Action {
+public interface Action extends Interfaces.Countable {
 	/**
 	 * @return 该 {@code Action} 块是否结束. 为真时,该 {@code Action} 未结束,反之亦然.
 	 */
@@ -16,5 +18,10 @@ public interface Action {
 	 */
 	default String paramsString() {
 		return String.valueOf(this.hashCode());
+	}
+
+	@Override
+	default long getCount() {
+		return 1;
 	}
 }
