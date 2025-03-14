b1605edf45228071cf93d92ae41d047228dbcb1f
Benny0Li <17826002105@163.com>
2025-03-14 11:23:53 +0800

    加入 ThreadedActionBuilder

---
+++ TeamCode/src/main/java/org/betastudio/ftc/action/builder/ThreadedActionBuilder.java
@@ -0,0 +1,25 @@
+package org.betastudio.ftc.action.builder;
+
+import org.betastudio.ftc.action.Action;
+import org.betastudio.ftc.action.utils.ThreadedAction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadedActionBuilder implements ActionBuilder{
+	protected final List <Action> actions;
+
+	public ThreadedActionBuilder(){
+		actions = new ArrayList <>();
+	}
+
+	@Override
+	public void append(Action action) {
+		actions.add(action);
+	}
+
+	@Override
+	public Action store() {
+		return new ThreadedAction(actions);
+	}
+}
