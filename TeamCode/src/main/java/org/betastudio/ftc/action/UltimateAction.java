67b2ba2dcce6ca4ef49009176f5027c768084da3
Benny0Li <17826002105@163.com>
2025-03-14 15:31:21 +0800

    加入 UltimateAction

---
+++ TeamCode/src/main/java/org/betastudio/ftc/action/UltimateAction.java
@@ -0,0 +1,6 @@
+package org.betastudio.ftc.action;
+
+/**
+ * 当有新的 Action 更新时，请确保在此处添加对应的接口，并在 ActionExpressions 类中声明。
+ */
+public interface UltimateAction extends ActionExpressions.NameableAction, ActionExpressions.PriorityAction {}
