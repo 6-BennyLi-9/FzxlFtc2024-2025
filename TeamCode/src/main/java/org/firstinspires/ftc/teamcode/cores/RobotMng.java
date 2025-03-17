24c5b6a5332b4c0c0988fad83f7e256828e7b03c
Benny0Li <17826002105@163.com>
2025-03-17 11:40:49 +0800

    在 RobotMng 中加入管理

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cores/RobotMng.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cores/RobotMng.java
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode.cores;
 
+import static org.betastudio.ftc.Interfaces.HardwareController;
+import static org.betastudio.ftc.Interfaces.InitializeRequested;
+import static org.betastudio.ftc.Interfaces.TagOptionsRequired;
+import static org.betastudio.ftc.Interfaces.Updatable;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.armScaleOperate;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.clipOption;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.decantOrSuspend;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.flipArm;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.highLowSpeedConfigChange;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.liftDecantUpping;
+import static org.firstinspires.ftc.teamcode.GamepadRequests.liftIDLE;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.liftSuspendLv2;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.liftSuspendPrepare;
-import static org.firstinspires.ftc.teamcode.GamepadRequests.liftIDLE;
+import static org.firstinspires.ftc.teamcode.GamepadRequests.ratchetTighten;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.sampleIO;
 import static org.firstinspires.ftc.teamcode.GamepadRequests.switchViewMode;
 import static org.firstinspires.ftc.teamcode.Global.gamepad1;
@@ -19,7 +24,6 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 
-import org.betastudio.ftc.Interfaces;
 import org.betastudio.ftc.action.Action;
 import org.betastudio.ftc.action.builder.TaggedThreadedActionBuilder;
 import org.betastudio.ftc.ui.client.Client;
@@ -38,6 +42,7 @@ import org.firstinspires.ftc.teamcode.cores.structure.ClipOp;
 import org.firstinspires.ftc.teamcode.cores.structure.DriveOp;
 import org.firstinspires.ftc.teamcode.cores.structure.LiftOp;
 import org.firstinspires.ftc.teamcode.cores.structure.PlaceOp;
+import org.firstinspires.ftc.teamcode.cores.structure.RatchetOp;
 import org.firstinspires.ftc.teamcode.cores.structure.RotateOp;
 import org.firstinspires.ftc.teamcode.cores.structure.ScaleOp;
 import org.firstinspires.ftc.teamcode.cores.structure.positions.LiftMode;
@@ -47,11 +52,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 机器人管理类，实现了 {@link Interfaces.Updatable} 接口。在使用此类时，需要先初始化硬件控制器 {@link #initControllers()}，
+ * 机器人管理类，实现了 {@link Updatable} 接口。在使用此类时，需要先初始化硬件控制器 {@link #initControllers()}，
  * 然后获取客户端 {@link #fetchClient()}。
  */
 @Config
-public class RobotMng implements Interfaces.Updatable {
+public class RobotMng implements Updatable {
 	/**
 	 * 打印代码的字符数组，用于在 telemetry 中显示状态更新
 	 */
@@ -67,7 +72,7 @@ public class RobotMng implements Interfaces.Updatable {
 	/**
 	 * 硬件控制器的映射表
 	 */
-	public final        Map <String, Interfaces.HardwareController> controllers         = new HashMap <>();
+	public final        Map <String, HardwareController> controllers         = new HashMap <>();
 	public              Action                                      hardwareAction;
 	/**
 	 * 更新时间，用于计算 telemetry 的更新状态
@@ -90,6 +95,7 @@ public class RobotMng implements Interfaces.Updatable {
 		controllers.put("rotate", new RotateOp());
 		controllers.put("scale", new ScaleOp());
 		controllers.put("drive", new DriveOp());
+		controllers.put("ratchet", new RatchetOp());
 	}
 
 	/**
@@ -113,18 +119,18 @@ public class RobotMng implements Interfaces.Updatable {
 	 */
 	public void initControllers() {
 		TaggedThreadedActionBuilder builder = new TaggedThreadedActionBuilder();
-		for (final Map.Entry <String, Interfaces.HardwareController> entry : controllers.entrySet()) {
+		for (final Map.Entry <String, HardwareController> entry : controllers.entrySet()) {
 			final String                        k = entry.getKey();
-			final Interfaces.HardwareController v = entry.getValue();
+			final HardwareController v = entry.getValue();
 
 			v.connect();
 			v.writeToInstance();
 
-			if (v instanceof Interfaces.InitializeRequested) {
-				((Interfaces.InitializeRequested) v).init();
+			if (v instanceof InitializeRequested) {
+				((InitializeRequested) v).init();
 			}
-			if (v instanceof Interfaces.TagOptionsRequired) {
-				((Interfaces.TagOptionsRequired) v).setTag(k + ":ctrl");
+			if (v instanceof TagOptionsRequired) {
+				((TagOptionsRequired) v).setTag(k + ":ctrl");
 			}
 
 			builder.append(k, v.getController());
@@ -226,6 +232,10 @@ public class RobotMng implements Interfaces.Updatable {
 			}
 		}
 
+		if (ratchetTighten.getEnabled()) {
+			RatchetOp.getInstance().tighten();
+		}
+
 		if (switchViewMode.getEnabled()) {
 			client.switchViewMode();
 			client.speak("The telemetry's ClientViewMode has recently switched to " + client.getCurrentViewMode());
@@ -277,7 +287,7 @@ public class RobotMng implements Interfaces.Updatable {
 
 		final String updateCode = "[" + printCode.charAt(updateTime % printCode.length()) + "]";
 
-		for (final Map.Entry <String, Interfaces.HardwareController> entry : controllers.entrySet()) {
+		for (final Map.Entry <String, HardwareController> entry : controllers.entrySet()) {
 			final String s = entry.getKey();
 			final Action a = entry.getValue().getController();
 			client.changeData(s, updateCode + a.paramsString());
