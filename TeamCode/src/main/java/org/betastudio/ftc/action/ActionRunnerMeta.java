77090c89a6cab4f1822dbcc71d59de04b723fc62
Benny0Li <17826002105@163.com>
2025-03-18 13:28:45 +0800

    加入 ActionRunnerMeta

---
+++ TeamCode/src/main/java/org/betastudio/ftc/action/ActionRunnerMeta.java
@@ -0,0 +1,39 @@
+package org.betastudio.ftc.action;
+
+import org.betastudio.ftc.Interfaces;
+import org.betastudio.ftc.util.Labeler;
+import org.betastudio.ftc.util.ProgressMarker;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ActionRunnerMeta implements Action {
+	private final Action metaRunner;
+
+	public ActionRunnerMeta(Action action, Interfaces.ProgressRender render) {
+		AtomicReference <Interfaces.ProgressMarker> marker = new AtomicReference <>(new ProgressMarker(action.getCount()));
+		AtomicReference <String>                    name   = new AtomicReference <>(Labeler.gen().summon(action));
+
+		if (action instanceof Interfaces.Nameable) {
+			name.set(((Interfaces.Nameable) action).getName());
+		}
+
+		if (action instanceof Interfaces.ProgressedTask) {
+			metaRunner = () -> {
+				marker.set(((Interfaces.ProgressedTask) action).getWorkerProgress());
+				render.render(name.get(), marker.get());
+				return action.activate();
+			};
+		} else {
+			metaRunner = () -> {
+				marker.get().tick();
+				render.render(name.get(), marker.get());
+				return action.activate();
+			};
+		}
+	}
+
+	@Override
+	public boolean activate() {
+		return metaRunner.activate();
+	}
+}
