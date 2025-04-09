d335b1ee2cf2c90e23efaaa1b1d55d002f5a2f47
Benny0Li <17826002105@163.com>
2025-04-09 14:21:47 +0800

    移动软件包

--- TeamCode/src/main/java/org/firstinspires/ftc/opmodes/ViewModeConvertor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/opmodes/ViewModeConvertor.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.opmodes.others;
+package org.firstinspires.ftc.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
