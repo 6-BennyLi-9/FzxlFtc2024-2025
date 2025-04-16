1393089ea791f59b6f4d10c95e1f84368791e212
Benny0Li <17826002105@163.com>
2025-04-16 14:27:44 +0800

    整理

--- TeamCode/src/main/java/org/firstinspires/ftc/opmodes/autonomous/UtilPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/opmodes/autonomous/UtilPoses.java
@@ -1,12 +1,20 @@
 package org.firstinspires.ftc.opmodes.autonomous;
 
-import static java.lang.Math.toRadians;
+import androidx.annotation.NonNull;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
+import org.jetbrains.annotations.Contract;
+
 public final class UtilPoses {
-	public static final Pose2d LeftStart       = new Pose2d(38, 60, toRadians(- 90));
-	public static final Pose2d Decant          = new Pose2d(59, 55, toRadians(- 135));
-	public static final Pose2d LeftSample      = new Pose2d(59, 50, toRadians(- 90));
-	public static final Pose2d LeftParkPrepare = new Pose2d(36, 10, toRadians(0));
+	public static final Pose2d LeftStart       = p(38, 60, - 90);
+	public static final Pose2d Decant          = p(59, 55, - 135);
+	public static final Pose2d LeftSample      = p(59, 50, - 90);
+	public static final Pose2d LeftParkPrepare = p(36, 10, 0);
+
+	@NonNull
+	@Contract("_, _, _ -> new")
+	private static Pose2d p(final double x, final double y, final double h) {
+		return new Pose2d(x, y, Math.toRadians(h));
+	}
 }
