7ab4bd4482c98840332f63d5ba1310b9f8b3d86a
Benny0Li <17826002105@163.com>
2025-04-16 14:20:35 +0800

    整理

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfigures.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfigures.java
@@ -26,8 +26,8 @@ public final class HardwareConfigures {
 	public static final double ROTATE_DEFAULT           = 0.79;
 	public static final double SCALE_MIN_POSITION       = 0.08;
 	public static final double SCALE_MAX_POSITION       = 0.5;
-	public static final double RATCHET_LOOSEN           = 0.05;
-	public static final double RATCHET_TIGHT            = 0.8;
 	public static final double SCALE_PROBE              = 0.5;
 	public static final double SCALE_BACH               = 0.08;
+	public static final double RATCHET_LOOSEN           = 0.05;
+	public static final double RATCHET_TIGHT            = 0.8;
 }
