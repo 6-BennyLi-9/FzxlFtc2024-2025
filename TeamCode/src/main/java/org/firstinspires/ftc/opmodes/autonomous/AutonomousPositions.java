7f1295ed2c9f17b1840a5f889faf5bb4441180d0
Benny0Li <17826002105@163.com>
2025-04-16 14:55:23 +0800

    static import

--- TeamCode/src/main/java/org/firstinspires/ftc/opmodes/autonomous/AutonomousPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/opmodes/autonomous/AutonomousPositions.java
@@ -1,13 +1,13 @@
 package org.firstinspires.ftc.opmodes.autonomous;
 
+import static org.betastudio.ftc.util.Pose2dUtil.p;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
-import org.betastudio.ftc.util.Pose2dUtil;
-
 public final class AutonomousPositions {
-	public static final Pose2d LeftStart       = Pose2dUtil.p(38, 60, - 90);
-	public static final Pose2d Decant          = Pose2dUtil.p(59, 55, - 135);
-	public static final Pose2d LeftSample      = Pose2dUtil.p(59, 50, - 90);
-	public static final Pose2d LeftParkPrepare = Pose2dUtil.p(36, 10, 0);
+	public static final Pose2d LeftStart       = p(38, 60, - 90);
+	public static final Pose2d Decant          = p(59, 55, - 135);
+	public static final Pose2d LeftSample      = p(59, 50, - 90);
+	public static final Pose2d LeftParkPrepare = p(36, 10, 0);
 
 }
