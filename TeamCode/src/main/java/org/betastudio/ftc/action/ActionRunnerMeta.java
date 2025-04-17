56d009ca45c8037c0c1efdf0346aa2ecef8eacbc
Benny0Li <17826002105@163.com>
2025-04-17 12:15:18 +0800

    fix(action): 修复进度任务结束后未更新标记和渲染的问题

--- TeamCode/src/main/java/org/betastudio/ftc/action/ActionRunnerMeta.java
+++ TeamCode/src/main/java/org/betastudio/ftc/action/ActionRunnerMeta.java
@@ -1,35 +1,51 @@
 package org.betastudio.ftc.action;
 
+import static org.betastudio.ftc.Interfaces.Nameable;
+import static org.betastudio.ftc.Interfaces.ProgressMarker;
+import static org.betastudio.ftc.Interfaces.ProgressRender;
+import static org.betastudio.ftc.Interfaces.ProgressedTask;
+
 import androidx.annotation.NonNull;
 
-import org.betastudio.ftc.Interfaces;
 import org.betastudio.ftc.util.Labeler;
 import org.betastudio.ftc.util.ProgressMarkerImplement;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 public final class ActionRunnerMeta implements Action {
 	private final Action metaRunner;
 
-	public ActionRunnerMeta(@NonNull final Action action, final Interfaces.ProgressRender render) {
-		final AtomicReference <Interfaces.ProgressMarker> marker = new AtomicReference <>(new ProgressMarkerImplement(action.getCount()));
-		final AtomicReference <String>                    name   = new AtomicReference <>(Labeler.gen().summon(action));
+	public ActionRunnerMeta(@NonNull final Action action, final ProgressRender render) {
+		final AtomicReference <ProgressMarker> marker = new AtomicReference <>(new ProgressMarkerImplement(action.getCount()));
+		final AtomicReference <String>         name   = new AtomicReference <>(Labeler.gen().summon(action));
 
-		if (action instanceof Interfaces.Nameable) {
-			name.set(((Interfaces.Nameable) action).getName());
+		if (action instanceof Nameable) {
+			name.set(((Nameable) action).getName());
 		}
 
-		if (action instanceof Interfaces.ProgressedTask) {
+		AtomicBoolean res = new AtomicBoolean(false);
+		if (action instanceof ProgressedTask) {
 			metaRunner = () -> {
-				marker.set(((Interfaces.ProgressedTask) action).getWorkerProgress());
+				marker.set(((ProgressedTask) action).getWorkerProgress());
 				render.render(name.get(), marker.get());
-				return action.activate();
+				res.set(action.activate());
+				if (! res.get()) {
+					marker.set(((ProgressedTask) action).getWorkerProgress());
+					render.render(name.get(), marker.get());
+				}
+				return res.get();
 			};
 		} else {
 			metaRunner = () -> {
 				marker.get().tick();
 				render.render(name.get(), marker.get());
-				return action.activate();
+				res.set(action.activate());
+				if (! res.get()) {
+					marker.get().tick();
+					render.render(name.get(), marker.get());
+				}
+				return res.get();
 			};
 		}
 	}
