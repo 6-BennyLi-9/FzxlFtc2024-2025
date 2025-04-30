ddf4643d6273165e9aa266d50430404ec9477880
Benny0Li <17826002105@163.com>
2025-04-30 14:51:51 +0800

    发现移动过程存在飘逸

--- TeamCode/src/main/java/org/firstinspires/ftc/opmodes/autonomous/Right.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/opmodes/autonomous/Right.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.opmodes.autonomous;
 import static org.betastudio.ftc.util.Pose2dUtil.xp;
 import static org.betastudio.ftc.util.Pose2dUtil.yp;
 import static org.firstinspires.ftc.opmodes.autonomous.AutonomousPositions.GET_SUSPEND;
+import static org.firstinspires.ftc.opmodes.autonomous.AutonomousPositions.RIGHT_PARK;
 import static org.firstinspires.ftc.opmodes.autonomous.AutonomousPositions.RIGHT_SAMPLE;
 import static org.firstinspires.ftc.opmodes.autonomous.AutonomousPositions.RIGHT_START;
 import static org.firstinspires.ftc.opmodes.autonomous.AutonomousPositions.SUSPEND;
@@ -20,9 +21,10 @@ import java.util.List;
 @Config
 @Autonomous(preselectTeleOp = "19419", group = "1_Beta")
 public class Right extends ActionBasedAutonomous {
-	public static final double        GET_SAMPLE_DISTANCE     = 3;
+	public static final double        GET_SAMPLE_DISTANCE     = 2;
 	public static final double        INTAKE_SAMPLE_DISTANCE  = - 5;
-	public static final double        SUSPEND_SAMPLE_DISTANCE = 8;
+	public static final double        SUSPEND_SAMPLE_DISTANCE = 5;
+	public static final double        SUSPEND_Y_CALIBRATE     = 1;
 	public static final int           SUSPEND_COUNT           = 2;
 	public static final int           INTAKE_COUNT            = 2;
 	public static final List <Pose2d> SUSPEND_POSES           = new ArrayList <>();
@@ -30,11 +32,11 @@ public class Right extends ActionBasedAutonomous {
 
 	static {
 		for (int i = 0 ; i < SUSPEND_COUNT ; i++) {
-			SUSPEND_POSES.add(xp(SUSPEND, SUSPEND_SAMPLE_DISTANCE));
+			SUSPEND_POSES.add(yp(xp(SUSPEND, SUSPEND_SAMPLE_DISTANCE * (i + 1)), SUSPEND_Y_CALIBRATE * i));
 		}
 
 		for (int i = 0 ; i < INTAKE_COUNT ; i++) {
-			INTAKE_POSES.add(xp(RIGHT_SAMPLE, INTAKE_SAMPLE_DISTANCE));
+			INTAKE_POSES.add(xp(RIGHT_SAMPLE, INTAKE_SAMPLE_DISTANCE * i));
 		}
 	}
 
@@ -42,34 +44,36 @@ public class Right extends ActionBasedAutonomous {
 	public void actionBuildEntry() {
 		utils.closeClip();
 		Actions.runAction(utils.pack());
-		appendSuspend();
+		appendSuspend(SUSPEND);
 
 		for (int i = 0 ; i < SUSPEND_COUNT ; i++) {
-			appendGetSample();
-			appendSuspend();
+			appendGetSample(GET_SUSPEND);
+			appendSuspend(SUSPEND_POSES.get(i));
 		}
 
-		executeAssembled(track.runTo(GET_SUSPEND), utils.pack());
+		executeAssembled(track.runTo(RIGHT_PARK), utils.pack());
 	}
 
 	/**
 	 * 自动预载电梯下的动作
 	 */
-	public void appendSuspend() {
+	public void appendSuspend(final Pose2d suspend) {
+		utils.waitMs(200);
 		utils.liftSuspendHighPrepare();
-		executeAssembled(track.runTo(SUSPEND), utils.pack());
+		executeAssembled(track.runTo(suspend), utils.pack());
 		utils.liftSuspendHigh();
 		utils.openClip();
 		executeManager();
+		utils.waitMs(200);
 		utils.liftDown();
 	}
 
-	public void appendGetSample() {
-		executeAssembled(track.runTo(GET_SUSPEND), utils.pack());
+	public void appendGetSample(final Pose2d get) {
+		executeAssembled(track.runTo(get), utils.pack());
 		utils.waitMs(500);
 		utils.closeClip();
 		utils.waitMs(500);
-		executeLinked(track.runTo(yp(GET_SUSPEND, GET_SAMPLE_DISTANCE)), utils.pack());
+		executeLinked(track.runTo(yp(get, GET_SAMPLE_DISTANCE)), utils.pack());
 	}
 
 	@Override
