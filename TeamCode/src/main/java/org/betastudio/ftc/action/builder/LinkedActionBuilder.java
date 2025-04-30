40897097f6dcd1095ca2b9ccedf0ed9ef5bad686
Benny0Li <17826002105@163.com>
2025-04-30 17:36:38 +0800

    javadoc

--- TeamCode/src/main/java/org/betastudio/ftc/action/builder/LinkedActionBuilder.java
+++ TeamCode/src/main/java/org/betastudio/ftc/action/builder/LinkedActionBuilder.java
@@ -28,6 +28,9 @@ public class LinkedActionBuilder implements ActionBuilder {
 		actions.remove(action);
 	}
 
+	/**
+	 * 为了防止后期出现 {@link IndexOutOfBoundsException}， 将提前断言可能出现的问题
+	 */
 	@Override
 	public Action store() {
 		assert ! actions.isEmpty();
