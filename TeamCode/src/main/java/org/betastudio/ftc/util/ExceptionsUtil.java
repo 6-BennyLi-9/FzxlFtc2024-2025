644e1fe03318db55e7871267d3631f8625d1a904
Benny0Li <17826002105@163.com>
2025-04-30 17:17:59 +0800

    javadoc

--- TeamCode/src/main/java/org/betastudio/ftc/util/ExceptionsUtil.java
+++ TeamCode/src/main/java/org/betastudio/ftc/util/ExceptionsUtil.java
@@ -3,6 +3,10 @@ package org.betastudio.ftc.util;
 import androidx.annotation.NonNull;
 
 public final class ExceptionsUtil {
+	/**
+	 * 用于跟踪异常的真正的原因
+	 * @return 异常的真正的抛出原因，不会有 {@link RuntimeException} 的干扰
+	 */
 	@NonNull
 	public static Throwable getOriginException(@NonNull Throwable e) {
 		if (e.getCause() == null) {
