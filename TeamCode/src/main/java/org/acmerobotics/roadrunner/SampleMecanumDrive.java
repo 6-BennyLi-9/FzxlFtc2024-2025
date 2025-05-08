693be2bff4a6c4852dd7ebaae0232b33fee457d2
Benny0Li <17826002105@163.com>
2025-05-08 13:03:15 +0800

    kD

--- TeamCode/src/main/java/org/acmerobotics/roadrunner/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/acmerobotics/roadrunner/SampleMecanumDrive.java
@@ -30,10 +30,12 @@ import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationCon
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
@@ -55,8 +57,8 @@ import java.util.List;
 public class SampleMecanumDrive extends MecanumDrive {
 	private static final TrajectoryVelocityConstraint     VEL_CONSTRAINT     = getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH);
 	private static final TrajectoryAccelerationConstraint ACCEL_CONSTRAINT   = getAccelerationConstraint(MAX_ACCEL);
-	public static        PIDCoefficients                  TRANSLATIONAL_PID  = new PIDCoefficients(8, 0, 0);
-	public static        PIDCoefficients                  HEADING_PID        = new PIDCoefficients(8, 0, 0);
+	public static        PIDCoefficients                  TRANSLATIONAL_PID  = new PIDCoefficients(8, 0, 1.8);
+	public static        PIDCoefficients                  HEADING_PID        = new PIDCoefficients(8, 0, 0.05);
 	public static        double                           LATERAL_MULTIPLIER = 1;
 	public static        double                           VX_WEIGHT          = 1;
 	public static        double                           VY_WEIGHT          = 1;
@@ -85,6 +87,10 @@ public class SampleMecanumDrive extends MecanumDrive {
 		for (final LynxModule module : hardwareMap.getAll(LynxModule.class)) {
 			module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
 		}
+
+		IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
+		IMU            imu        = hardwareMap.get(IMU.class, "imu");
+		imu.initialize(parameters);
 
 		//DONE_TODO: adjust the names of the following hardware devices to match your configuration
 
@@ -188,8 +194,9 @@ public class SampleMecanumDrive extends MecanumDrive {
 	public void update() {
 		updatePoseEstimate();
 		final DriveSignal signal = trajectorySequenceRunner.update(getPoseEstimate(), getPoseVelocity());
-		if (null != signal)
+		if (null != signal) {
 			setDriveSignal(signal);
+		}
 	}
 
 	public void waitForIdle() {
