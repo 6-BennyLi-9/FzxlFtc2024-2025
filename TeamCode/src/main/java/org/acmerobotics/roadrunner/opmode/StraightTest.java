ffbcc2b5dca6d4d90379e48ff72ac50dfae86339
Benny0Li <17826002105@163.com>
2025-05-08 12:17:54 +0800

    Revert "Disabled"
    This reverts commit b2cf0280bd3b6e46e5291a142891d7e3a8585fde.

--- TeamCode/src/main/java/org/acmerobotics/roadrunner/opmode/StraightTest.java
+++ TeamCode/src/main/java/org/acmerobotics/roadrunner/opmode/StraightTest.java
@@ -6,7 +6,6 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.acmerobotics.roadrunner.SampleMecanumDrive;
@@ -17,9 +16,9 @@ import org.firstinspires.ftc.teamcode.Local;
  * This is a simple routine to test translational drive capabilities.
  */
 
-@Disabled
 @Config
 @Autonomous(group = "drive")
+//@Disabled
 public class StraightTest extends LinearOpMode {
 	public static final double DISTANCE = 60; // in
 
