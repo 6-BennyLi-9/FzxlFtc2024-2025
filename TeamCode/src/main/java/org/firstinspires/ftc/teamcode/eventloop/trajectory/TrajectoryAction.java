53200bf64ef082babf2e6d1a8fa7a17407076be4
Benny0Li <17826002105@163.com>
2025-05-16 12:32:35 +0800

    实现 ProgressedTask

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eventloop/trajectory/TrajectoryAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eventloop/trajectory/TrajectoryAction.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.eventloop.trajectory;
 
+import static org.betastudio.ftc.Interfaces.ProgressMarker;
+import static org.betastudio.ftc.Interfaces.ProgressedTask;
 import static org.betastudio.ftc.util.Pose2dUtil.str;
+
+import androidx.annotation.NonNull;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -9,36 +13,47 @@ import org.acmerobotics.roadrunner.SampleMecanumDrive;
 import org.acmerobotics.roadrunner.trajectorysequence.TrajectorySequence;
 import org.betastudio.ftc.action.ActionImplementFactory;
 import org.betastudio.ftc.ui.client.Client;
+import org.betastudio.ftc.util.Pose2dUtil;
+import org.betastudio.ftc.util.ProgressMarkerImplement;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * 不会自动调用 {@link SampleMecanumDrive#update()}
  */
-public class TrajectoryAction extends ActionImplementFactory {
+public class TrajectoryAction extends ActionImplementFactory implements ProgressedTask {
+	public static final double PROGRESS_LENGTH_MULTIPLIER = 10;
+
+	private final ProgressMarkerImplement marker;
 	private static Client client;
 
-	public TrajectoryAction(final SampleMecanumDrive drive, final TrajectorySequence trajectorySequence) {
+	public TrajectoryAction(@NonNull final SampleMecanumDrive drive, @NonNull final TrajectorySequence track) {
 		final AtomicBoolean isInitialized = new AtomicBoolean(false);
+		marker = new ProgressMarkerImplement((long) (PROGRESS_LENGTH_MULTIPLIER * Pose2dUtil.dis(track.end().minus(track.start()))));
+
 		setAction(() -> {
 			if (! isInitialized.get()) {
 				isInitialized.set(true);
-				drive.followTrajectorySequenceAsync(trajectorySequence);
+				drive.followTrajectorySequenceAsync(track);
 			}
-			Pose2d minus = trajectorySequence.end().minus(drive.getPoseEstimate());
+			Pose2d minus = track.end().minus(drive.getPoseEstimate());
+			marker.setDone((long) (PROGRESS_LENGTH_MULTIPLIER * Pose2dUtil.dis(minus)));
 			client.putData("点位差", str(minus));
 			return drive.isBusy();
 		});
 	}
 
-	public TrajectoryAction(final SampleMecanumDrive drive, final Trajectory trajectory) {
+	public TrajectoryAction(@NonNull final SampleMecanumDrive drive, @NonNull final Trajectory track) {
 		final AtomicBoolean isInitialized = new AtomicBoolean(false);
+		marker = new ProgressMarkerImplement((long) (PROGRESS_LENGTH_MULTIPLIER * Pose2dUtil.dis(track.end().minus(track.start()))));
+
 		setAction(() -> {
 			if (! isInitialized.get()) {
 				isInitialized.set(true);
-				drive.followTrajectoryAsync(trajectory);
+				drive.followTrajectoryAsync(track);
 			}
-			Pose2d minus = trajectory.end().minus(drive.getPoseEstimate());
+			Pose2d minus = track.end().minus(drive.getPoseEstimate());
+			marker.setDone((long) (PROGRESS_LENGTH_MULTIPLIER * Pose2dUtil.dis(minus)));
 			client.putData("点位差", str(minus));
 			return drive.isBusy();
 		});
@@ -46,5 +61,10 @@ public class TrajectoryAction extends ActionImplementFactory {
 
 	public static void setClient(final Client client) {
 		TrajectoryAction.client = client;
+	}
+
+	@Override
+	public ProgressMarker getWorkerProgress() {
+		return marker;
 	}
 }
