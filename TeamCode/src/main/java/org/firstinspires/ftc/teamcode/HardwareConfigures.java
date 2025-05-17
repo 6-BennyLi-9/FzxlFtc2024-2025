67d732700ed2e3c065e88ee53563803c94de6891
Benny0Li <17826002105@163.com>
2025-05-17 13:03:27 +0800

    claw 紧急调整

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfigures.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfigures.java
@@ -11,9 +11,9 @@ public final class HardwareConfigures {
 	public static double ARM_IDLE                 = 0.8;
 	public static double ARM_SAFE                 = 0.61;
 	public static double ARM_RISE                 = 0.37;
-	public static double CLAW_OPEN                = 0.42;
-	public static double CLAW_HALF_OPEN           = 0.52;
-	public static double CLAW_CLOSE               = 0.58;
+	public static double CLAW_OPEN                = 0.62;
+	public static double CLAW_HALF_OPEN           = 0.53;
+	public static double CLAW_CLOSE               = 0.45;
 	public static int    CLIP_OPEN                = 0;
 	public static double CLIP_CLOSE               = 0.5;
 	public static int    LIFT_DECANT_LOW          = 1080;
